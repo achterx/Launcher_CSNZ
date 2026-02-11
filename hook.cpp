f4f2b8461857477f8d3c947947579d90b4eacc37
achter <90976703+achterx@users.noreply.github.com>
2026-02-11 15:20:20 +0300

    Update hook.cpp

--- hook.cpp
+++ hook.cpp
@@ -1145,7 +1145,8 @@ CreateHookClass(const char*, GetSSLProtocolName)
 
 CreateHookClassType(void*, SocketConstructor, int, int a2, int a3, char a4)
 {
-    *(DWORD*)((int)ptr + 72) = (DWORD)g_pfnEVP_CIPHER_CTX_new();
+     MessageBox(NULL, "you foken stupid!", "Debug", MB_OK); // Did you see this?
+	*(DWORD*)((int)ptr + 72) = (DWORD)g_pfnEVP_CIPHER_CTX_new();
 	*(DWORD*)((int)ptr + 76) = (DWORD)g_pfnEVP_CIPHER_CTX_new();
 	*(DWORD*)((int)ptr + 80) = (DWORD)g_pfnEVP_CIPHER_CTX_new();
 	*(DWORD*)((int)ptr + 84) = (DWORD)g_pfnEVP_CIPHER_CTX_new();
